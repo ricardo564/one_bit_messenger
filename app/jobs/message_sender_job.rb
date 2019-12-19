8a518985dd50c9736e3e4dba3cd6dd3ba0f57640
ricardo564 <ricardo_hks@hotmail.com>
2019-12-19 11:15:12 -0300

    Recepção das Mensagens- Aula 3

---
+++ app/jobs/message_sender_job.rb
@@ -0,0 +1,15 @@
+class MessageSenderJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    # Do something later
+    MessageChannel.broadcast_to(message.receiver, render_partial(message))
+  end
+
+  private
+
+  def render_partial(message)
+    MessagesController.renderer.render partial: "messages/message",
+                                       locals: {message: message, current_user: message.receiver}
+  end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,10 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates :content, presence: true
+
+  after_create :send_message
+
+  def send_message
+    MessageSenderJob.perform_now(self)
+  end
 end
